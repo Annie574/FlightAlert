e29eb382d6d855b326cd6617ba7696a8b0d86124
Annie574 <88533090+Annie574@users.noreply.github.com>
2025-09-10 13:42:38 +0200

    Update main.py
    Delete data for debugging section

--- main.py
+++ main.py
@@ -19,38 +19,21 @@ manager = DataManager()
 flight_manager = FlightSearch()
 notification_manager = NotificationManager()
 
-# =============== Trial data for debugging ===============
-sheet_data = [
-    {'city': 'Frankfurt', 'iataCode': 'FRA', 'id': 3, 'lowestPrice': 500},
-    {'city': 'Paris', 'iataCode': 'PAR', 'id': 2, 'lowestPrice': 300},
-    {'city': 'Tokyo', 'iataCode': 'TYO', 'id': 4, 'lowestPrice': 485},
-    {'city': 'Hong Kong', 'iataCode': 'HKG', 'id': 5, 'lowestPrice': 551},
-    {'city': 'Istanbul', 'iataCode': 'IST', 'id': 6, 'lowestPrice': 95},
-    {'city': 'Kuala Lumpur', 'iataCode': 'KUL', 'id': 7, 'lowestPrice': 414},
-    {'city': 'New York', 'iataCode': 'NYC', 'id': 8, 'lowestPrice': 240},
-    {'city': 'San Francisco', 'iataCode': 'SFO', 'id': 9, 'lowestPrice': 260},
-    {'city': 'Dublin', 'iataCode': 'DBN', 'id': 10, 'lowestPrice': 378}
-]
-users_data = [
-    {'timestamp': '9/10/2025 8:53:54', 'whatIsYourFirstName?': 'Balbina', 'whatIsYourLastName?': 'Nowak', 'whatIsYourEmail?': 'b0889501@gmail.com'},
-    {'timestamp': '9/10/2025 8:54:19', 'whatIsYourFirstName?': 'Angela', 'whatIsYourLastName?': 'Kowalski', 'whatIsYourEmail?': 'angela@email.com'}
-]
-
 # =============== Update IATA codes in Google Sheet ===============
-# sheet_data = manager.get_prices_data()
-#
-# for row in sheet_data:
-#     if not row["iataCode"]:
-#         row["iataCode"] = flight_manager.search_IATA_code(row["city"])
-#         # slowing down requests to avoid rate limit
-#         time.sleep(2)
-#
-# manager.destination_data = sheet_data
-# manager.update_destination_codes()
+sheet_data = manager.get_prices_data()
+
+for row in sheet_data:
+    if not row["iataCode"]:
+        row["iataCode"] = flight_manager.search_IATA_code(row["city"])
+        # slowing down requests to avoid rate limit
+        time.sleep(2)
+
+manager.destination_data = sheet_data
+manager.update_destination_codes()
 
 # =============== Obtain users database from Google Sheet ===============
 
-# users_data = manager.get_customer_emails()
+users_data = manager.get_customer_emails()
 customer_email_list = [row["whatIsYourEmail?"] for row in users_data]
 # print(f"Your email list includes {customer_email_list}")
 
